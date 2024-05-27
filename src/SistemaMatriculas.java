44201c75a19113fbfd97f710ed7c05d7a08432ff
jud-as <guilhermeferreira082@gmail.com>
2024-05-27 15:23:33 -0300

    Criado programa-base.

--- src/SistemaMatriculas.java
+++ src/SistemaMatriculas.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SistemaMatriculas {
 
-    public static void main(String[] args) {
+    static Scanner input = new Scanner(System.in);
+    static ArrayList<Alunos> arrayAlunos;
+    static ArrayList<Alunos> arrayAlunosExcluidos;
 
+    public static void main(String[] args) {
+        arrayAlunos = new ArrayList<>();
+        arrayAlunosExcluidos = new ArrayList<>();
+        operacoes();
     }
 
     public static void operacoes() {
@@ -38,4 +48,92 @@ public class SistemaMatriculas {
                 break;
         }
     }
+
+    public static void cadastrarAluno() {
+
+        System.out.println("\nNome: ");
+        String nome = input.next();
+        System.out.println("\nSérie: ");
+        String serie = input.next();
+        System.out.println("\nPeríodo: ");
+        String periodo = input.next();
+        Alunos aluno = new Alunos(nome, serie, periodo);
+        if (!arrayAlunosExcluidos.isEmpty()) aluno.setMatricula(orderAlunos(arrayAlunosExcluidos);
+        arrayAlunos.add(aluno);
+        System.out.println("\nAluno cadastrado com sucesso.");
+        operacoes();
+    }
+
+    public static void excluirAluno() {
+
+        System.out.println("\nDigite a matrícula do aluno que deseja excluir: ");
+        int matricula = input.nextInt();
+        Alunos aluno = buscarAluno(matricula);
+        if (aluno != null) {
+            arrayAlunosExcluidos.add(aluno);
+            arrayAlunos.remove(aluno);
+            System.out.println("\nAluno excluído com sucesso.");
+        }
+        operacoes();
+    }
+
+    public static Alunos buscarAluno(int matricula) {
+        Alunos aluno = null;
+        if (!arrayAlunos.isEmpty()) {
+            for (Alunos a : arrayAlunos) {
+                if (a.getMatricula() == matricula) {
+                    aluno = a;
+                }else{
+                    System.out.println("\nAluno não encontrado.");
+                }
+            }
+        } else {
+            System.out.println("\nNão há alunos cadastrados.");
+        }
+        return aluno;
+    }
+
+    public static void atualizarMatricula() {
+        System.out.println("\nDigite a matrícula do aluno que deseja atualizar: ");
+        int matricula = input.nextInt();
+        Alunos aluno = buscarAluno(matricula);
+        if (aluno != null) {
+            System.out.println("\nDigite o novo nome: ");
+            String nome = input.next();
+            System.out.println("\nDigite a nova série: ");
+            String serie = input.next();
+            System.out.println("\nDigite o novo período: ");
+            String periodo = input.next();
+            aluno.setNome(nome);
+            aluno.setSerie(serie);
+            aluno.setPeriodo(periodo);
+            System.out.println("\nAluno atualizado com sucesso.");
+            operacoes();
+        }
+    }
+
+    public static void listarAlunos() {
+        if (!arrayAlunos.isEmpty()) {
+            for (Alunos a : arrayAlunos) {
+                System.out.println(a);
+            }
+
+        }else{
+            System.out.println("\nNão há alunos cadastrados.");
+        }
+        operacoes();
+    }
+
+    public static int orderAlunos(ArrayList<Alunos> arrayAlunos) {
+        int matricula = 0;
+        ArrayList<int> matriculas = new ArrayList<int>();
+        for (Alunos a : arrayAlunos) {
+            matriculas.add(a.getMatricula());
+        }
+        matricula = matriculas.getFirst();
+        matriculas.removeFirst();
+        return matricula;
+    }
+
+
 }
